563a4504e3da7cdfec8215535e4637ec23f528e2
F-H-P <98205827+F-H-P@users.noreply.github.com>
2023-07-31 17:37:57 +0700

    test

--- robot_description/run.sh
+++ robot_description/run.sh
@@ -1,14 +1,15 @@
-cd
-cd IDC/IDC_ws/
-colcon build --symlink-install
-source /opt/ros/humble/setup.bash
-source install/setup.bash
+# cd
+# cd IDC/IDC_ws/
+# colcon build --symlink-install
+# source /opt/ros/humble/setup.bash
+# source install/setup.bash
 
-cd src/IDC-Simulation/robot_description/config/
+cd
+cd IDC/IDC_ws/src/IDC-Simulation/robot_description/config/
 id=$(niet "TeamNo" properties.yaml)
 cp -r ~/IDC/properties.yaml ~/IDC/IDC_ws/src/IDC-Simulation/robot_description/config/properties.yaml
-
+# ROS_DOMAIN_ID=$id 
 cd ../../..
 cd IDC_Webserver/
-gnome-terminal -x sh -c "ROS_DOMAIN_ID=$id ros2 launch robot_description robot_gazebolaunch.py; exec bash" &
-gnome-terminal -x sh -c "ROS_DOMAIN_ID=$id node client.js; exec bash"
+gnome-terminal -x sh -c "ros2 launch robot_description robot_gazebolaunch.py; exec bash" ;
+gnome-terminal -x sh -c "node client.js; exec bash"
