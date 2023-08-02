d1d8bfca66cd97bf892ea8e5f95806d1a3944a00
F-H-P <98205827+F-H-P@users.noreply.github.com>
2023-08-02 15:01:22 +0700

    add ros bag

--- robot_description/run.sh
+++ robot_description/run.sh
@@ -1,17 +1,17 @@
-# cd
-# cd IDC/IDC_ws/
-# colcon build --symlink-install
-# source /opt/ros/humble/setup.bash
-# source install/setup.bash
+cd
+cd IDC/IDC_ws/
+source /opt/ros/humble/setup.bash
+source install/setup.bash
 
 cd
-cd IDC
-rm -r ros_bag
-cd IDC_ws/src/IDC-Simulation/robot_description/config/
+cd IDC/IDC_ws/src/IDC-Simulation/robot_description/config/
 id=$(niet "TeamNo" properties.yaml)
 cp -r ~/IDC/properties.yaml ~/IDC/IDC_ws/src/IDC-Simulation/robot_description/config/properties.yaml
-# ROS_DOMAIN_ID=$id 
+ROS_DOMAIN_ID=$id
 cd ../../..
 cd IDC_Webserver/
 gnome-terminal -x bash -c "ROS_DOMAIN_ID=$id ros2 launch robot_description robot_gazebolaunch.py; exec bash" ;
-gnome-terminal -x bash -c "ROS_DOMAIN_ID=$id node client.js; exec bash"
+gnome-terminal -x bash -c "ROS_DOMAIN_ID=$id node client.js; exec bash";
+cd
+cd IDC
+ROS_DOMAIN_ID=$id ros2 run robot_description ros_bag.py
